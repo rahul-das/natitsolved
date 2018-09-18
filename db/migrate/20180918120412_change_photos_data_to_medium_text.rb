3f092e05c7f4045c1d385bed47993090d6b53c24
Rahul Kumar Das <rahulkumardas@Rahuls-iMac.local>
2018-09-18 19:29:30 +0530

    mysql -> pg

--- db/migrate/20180918120412_change_photos_data_to_medium_text.rb
+++
@@ -1,5 +0,0 @@
-class ChangePhotosDataToMediumText < ActiveRecord::Migration[5.1]
-  def change
-      change_column :photos, :data, :text, limit: 16.megabytes - 1
-  end
-end
--- db/migrate/20180918120643_change_photos_data_to_binary.rb
+++
@@ -1,5 +0,0 @@
-class ChangePhotosDataToBinary < ActiveRecord::Migration[5.1]
-  def change
-      change_column :photos, :data, :binary, limit: 16.megabytes - 1
-  end
-end
